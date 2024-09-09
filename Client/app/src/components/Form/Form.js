997dbc075fd12d08cf5a8d0e90674755dd7846ce
Victor Mikhael Coelho Pomaroli <Victor.Pomaroli@topcred.com>
2024-09-09 10:46:51 -0300

    form pronto

---
+++ Client/app/src/components/Form/Form.js
@@ -0,0 +1,18 @@
+import "./Form.css"
+
+export default function Form() {
+    return (
+    <div className="box">
+        <h1>Form Test</h1>
+        <div className="campo">
+            <p>Usuário</p>
+            <input type="name"/>
+        </div>
+        <div className="campo">
+            <p>Senha</p>
+            <input type="password"/>
+        </div>
+        <button>Enviar</button>
+    </div>
+    )
+}
